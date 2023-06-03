debfa63da8202bbbfd0d201c306a9315ade708cc
Michal <89008258+Michal-6@users.noreply.github.com>
2023-06-03 14:04:24 +0200

    Fixed hamburger menu

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 /*-----------------------------------------------------------*/
+function myFunction() { 
+                var x = document.getElementById("dropdown_nav"); 
+                if (x.style.display == "none") {
+                x.style.display = "block";
+                x.style.animationDirection="normal";
+                x.className = 'drop_anim';
+              } else {
+                x.style.animationDirection="reverse";
+                x.className = 'drop_anim';
+                setTimeout(() => {x.style.display = "none";}, "300")
+              }
+    setTimeout(() => {x.className = '';}, "300")
+}
